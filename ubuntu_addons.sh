a2ba913a458b15acdb054234726bd08b8402cb0c
ridgegate <tom.liu@ridgegatetools.ca>
2018-06-22 11:16:35 -0700

    Update ubuntu_addons.sh

--- ubuntu_addons.sh
+++ ubuntu_addons.sh
@@ -2,13 +2,15 @@
 # This script install the following packages
 #   1. xRDP, xfce4 interface for windows remote desktop connections
 #   2. Fsearch for quick file search like Everything from Voidtools for Windows
+#   3. Syncthing for file synchronization on your own computers without cloud services 
+#      (similar to Dropbox but requires computesr to be always on)
 #
 # sources:
 # https://www.tweaking4all.com/software/linux-software/use-xrdp-remote-access-ubuntu-14-04/
 # https://github.com/cboxdoerfer/fsearch
 
 clear 
-# Installing first step
+# -------Install xRDP and xfce4-------
 echo "installing the required packages"
 sudo apt-get update
 sudo apt-get install xrdp
@@ -21,8 +23,20 @@ cp /etc/xrdp/startwm.sh /etc/xrdp/startwm.sh_backup
 wget -q https://raw.githubusercontent.com/ridgegate/xRDP-on-Ubuntu/master/startwm.sh -O /etc/xrdp/startwm.sh 
 
 sudo service xrdp restart
+# -------End of Install xRDP and xfce4-------
 
-#install the second step: fsearch
+# -------Install fsearch-------
 sudo add-apt-repository ppa:christian-boxdoerfer/fsearch-daily
 sudo apt-get update
 sudo apt install fsearch-trunk
+# -------End of Install fsearch-------
+
+# -------Install Syncthing-------
+# Add the release PGP keys:
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+# Add the "stable" channel to your APT sources:
+echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+# Update and install syncthing:
+sudo apt-get update
+sudo apt-get install syncthing
+# -------End of Install Syncthing-------
