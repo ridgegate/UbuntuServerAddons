efe1ba56697e083540324b946a3140ae745221d6
ridgegate <tom.liu@ridgegatetools.ca>
2018-06-22 12:54:54 -0700

    Update ubuntu_addons.sh

--- ubuntu_addons.sh
+++ ubuntu_addons.sh
@@ -41,3 +41,10 @@ echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sourc
 sudo apt-get update -y
 sudo apt-get install -y syncthing
 # -------End of Install Syncthing-------
+
+# -------Install Chrome-------
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update -y
+sudo apt-get install -y google-chrome-stable
+# -------End of Install Chrome-------
