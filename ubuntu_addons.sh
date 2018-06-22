d857164309c04dcad1089d59eaa14116aad313bf
ridgegate <tom.liu@ridgegatetools.ca>
2018-06-22 16:39:43 -0700

    Update ubuntu_addons.sh

--- ubuntu_addons.sh
+++ ubuntu_addons.sh
@@ -5,21 +5,32 @@
 #   3. Syncthing for file synchronization on your own computers without cloud services 
 #      (similar to Dropbox but requires computesr to be always on)
 #   4. Chrome for accessing Syncthing WebGUI without allowing anyone to connect even from WWW
+#   5. Samba for windows file sharing
 #
 # sources:
 # https://www.tweaking4all.com/software/linux-software/use-xrdp-remote-access-ubuntu-14-04/
 # https://github.com/cboxdoerfer/fsearch
 # https://apt.syncthing.net/
 
+# --------Add all the repositories-------
+#FSearch Repository
+sudo add-apt-repository -y ppa:christian-boxdoerfer/fsearch-daily
+#Syncthing Repository
+curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
+echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
+#Chrome Repository
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update -y
+
 clear 
 echo "Please provide your username"
 read -p "Type your user name, then press [ENTER] : " MY_USER_NAME
 echo "Please provide your password"
-read -p "Type your user name, then press [ENTER] : " MY_PWD
+read -p "Type your password, then press [ENTER] : " MY_PWD
+echo "installing the required packages"
 
 # -------Install xRDP and xfce4-------
-echo "installing the required packages"
-sudo apt-get update -y
 sudo apt-get install -y xrdp
 sudo apt-get install -y xfce4
 
@@ -32,16 +43,7 @@ wget -q https://raw.githubusercontent.com/ridgegate/xRDP-on-Ubuntu/master/startw
 sudo service xrdp restart
 # -------End of Install xRDP and xfce4-------
 
-# -------Install fsearch-------
-sudo add-apt-repository -y ppa:christian-boxdoerfer/fsearch-daily
-sudo apt-get update -y
-sudo apt install -y fsearch-trunk
-# -------End of Install fsearch-------
-
 # -------Install Syncthing-------
-curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
-echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
-sudo apt-get update -y
 sudo apt-get install -y syncthing
 
 #Autostart Syncthing as systemd service
@@ -51,9 +53,32 @@ systemctl enable syncthing@$MY_USER_NAME.service
 systemctl start syncthing@$MY_USER_NAME.service
 # -------End of Install Syncthing-------
 
-# -------Install Chrome-------
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo apt-get update -y
+# ------- Install Samba-------
+sudo apt-get install -y samba smbclient
+sudo smbpasswd -a $MY_USER_NAME
+expect \"New SMB password:\"
+send \"$MY_PWD\r\"
+expect \"Retype new SMB password:\"
+send \"$MY_PWD\r\"
+# -------End of Install Samba-------
+
+# Install fsearch
+sudo apt install -y fsearch-trunk
+# Install Chrome
 sudo apt-get install -y google-chrome-stable
-# -------End of Install Chrome-------
+# Install General Purporse Mouse
+sudo apt-get install -y gpm
+
+echo "Edit your /etc/samba/smb.conf file to allow access to the appropriate folders/files"
+echo "Format to follow:"
+echo "    [<folder_name>]"
+echo "    path = <path>/<folder_name>"
+echo "    valid users = <user_name>"
+echo "    read only = no"
+echo "    browsable = yes"
+echo
+echo "Once smb.conf file has been edited and saved. Restart SAMBA service with the following command"
+ehco "sudo service smbd restart"
+echo
+echo
+echo "All done and Enjoy!"
