3fca496dc4b1e96b63eb633fbd9ee5379ec0f191
ridgegate <tom.liu@ridgegatetools.ca>
2018-06-22 15:50:48 -0700

    Update ubuntu_addons.sh

--- ubuntu_addons.sh
+++ ubuntu_addons.sh
@@ -4,6 +4,7 @@
 #   2. Fsearch for quick file search like Everything from Voidtools for Windows
 #   3. Syncthing for file synchronization on your own computers without cloud services 
 #      (similar to Dropbox but requires computesr to be always on)
+#   4. Chrome for accessing Syncthing WebGUI without allowing anyone to connect even from WWW
 #
 # sources:
 # https://www.tweaking4all.com/software/linux-software/use-xrdp-remote-access-ubuntu-14-04/
@@ -11,6 +12,11 @@
 # https://apt.syncthing.net/
 
 clear 
+echo "Please provide your username"
+read -p "Type your user name, then press [ENTER] : " MY_USER_NAME
+echo "Please provide your password"
+read -p "Type your user name, then press [ENTER] : " MY_PWD
+
 # -------Install xRDP and xfce4-------
 echo "installing the required packages"
 sudo apt-get update -y
@@ -33,13 +39,16 @@ sudo apt install -y fsearch-trunk
 # -------End of Install fsearch-------
 
 # -------Install Syncthing-------
-# Add the release PGP keys:
 curl -s https://syncthing.net/release-key.txt | sudo apt-key add -
-# Add the "stable" channel to your APT sources:
 echo "deb https://apt.syncthing.net/ syncthing stable" | sudo tee /etc/apt/sources.list.d/syncthing.list
-# Update and install syncthing:
 sudo apt-get update -y
 sudo apt-get install -y syncthing
+
+#Autostart Syncthing as systemd service
+sudo wget -q https://raw.githubusercontent.com/syncthing/syncthing/master/etc/linux-systemd/system/syncthing%40.service /etc/systemd/system/syncthing%40.service
+# Might need to enter password -- Could use expect to automate the process.
+systemctl enable syncthing@$MY_USER_NAME.service
+systemctl start syncthing@$MY_USER_NAME.service
 # -------End of Install Syncthing-------
 
 # -------Install Chrome-------
